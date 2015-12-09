1e7aa0c1a60eeefd307fddda38e5f060f1711f85
Tiziano Cappellari <tcappellari@nabertech.com>
2015-12-09 12:07:52 +0100

    hello world

---
+++ lesson-01/HelloWorld.java
@@ -0,0 +1,18 @@
+
+// class definition
+class HelloWorld {
+	// class body
+
+	// fields
+	// [...]
+
+
+	// methods
+	// [...]
+	public static void main(String[] args) {
+		//
+		System.out.println("Hello, world!");
+	}
+
+	// end class body
+} // class end
