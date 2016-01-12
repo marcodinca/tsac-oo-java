074e0a06de0e00139e761de91480a896650d9930
marcodinca <marcodinca@hotmail.it>
2016-01-12 10:24:25 +0100

    mlml

--- Compiti Vacanze/src/Alieno.java
+++
@@ -1,9 +0,0 @@
-public class Alieno {
-    public String nome;
-    public int danno;
-    
-    public Alieno(String nome, int danno) {
-        this.nome = nome;
-        this.danno = danno;
-    }
-}
--- Compiti Vacanze/src/Giocatore.java
+++
@@ -1,9 +0,0 @@
-public class Giocatore {
-    public String nome;
-    public int salute;
-    
-    public Giocatore(String nome) {
-        this.nome = nome;
-        this.salute = 100;
-    }
-}
